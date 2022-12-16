2a90a218ba612ce039f57a5e207910f5a4f7a3b0
Maxim <mr.maximgus@gmail.com>
2022-12-16 18:44:50 +0500

    Ready for delivery No. 1

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -2,17 +2,17 @@ import java.util.Random;
 
 public class MagicBox<T> {
 
-    T[] items = null;
+    T[] items;
 
-    public MagicBox () {
-         items = (T[]) new Object[5];
+    public MagicBox() {
+        this.items = (T[]) new Object[5];   
     }
 
-    public boolean add(T item){
-        if (checkingForSpace()){
+    public boolean add(T item) {
+        if (checkingForSpace()) {
             for (int i = 0; i < items.length; i++) {
-                if (items[i] == null) {
-                    items[i] = item;
+                if (this.items[i] == null) {
+                    this.items[i] = item;
                     return true;
                 }
             }
@@ -20,24 +20,30 @@ public class MagicBox<T> {
         return false;
     }
 
-    public T pick(){
-        if (!checkingForSpace()){
+    public T pick() {
+        if (!checkingForSpace()) {
             Random random = new Random();
             int randomInt = random.nextInt(5); // джава подберёт случайное число от 0 до ЧИСЛО невключительно
-            return items[randomInt];
+            return this.items[randomInt];
+        } else {
+            int count = 0;
+            for (int i = 0; i < items.length; i++) {
+                if (this.items[i] == null) {
+                    count++;
+                }
+            }
+
+            throw new RuntimeException("В коробке есть ещё " + count + " место!!! Заполните и его.");
         }
-        else{
-            RuntimeException runtimeException = new RuntimeException("Коробка ещё не полна");
-        }
-        return null;
+
     }
 
-    private boolean checkingForSpace () {
-        for (T item : items) {
+    private boolean checkingForSpace() {
+        for (T item : this.items) {
             if (item == null) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MagicBox<String> magicBox = new MagicBox<>();
+        MagicBox<Integer> magicBox2 = new MagicBox<>();
+
+        magicBox.add("a");
+        magicBox.add("b");
+        magicBox.add("c");
+        magicBox.add("d");
+        magicBox.add("e");
+        System.out.println(magicBox.pick());
+
+//        magicBox2.add(1);
+        magicBox2.add(2);
+        magicBox2.add(3);
+        magicBox2.add(4);
+        magicBox2.add(5);
+        System.out.println(magicBox2.pick());
     }
 }
