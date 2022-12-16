5836c3f5af174ed27090871ca5b2c8a8197ff463
Maxim <mr.maximgus@gmail.com>
2022-12-16 18:22:44 +0500

    Methods created (requires checking and debugging)

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,2 +1,43 @@
-public class MagicBox {
+import java.util.Random;
+
+public class MagicBox<T> {
+
+    T[] items = null;
+
+    public MagicBox () {
+         items = (T[]) new Object[5];
+    }
+
+    public boolean add(T item){
+        if (checkingForSpace()){
+            for (int i = 0; i < items.length; i++) {
+                if (items[i] == null) {
+                    items[i] = item;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public T pick(){
+        if (!checkingForSpace()){
+            Random random = new Random();
+            int randomInt = random.nextInt(5); // джава подберёт случайное число от 0 до ЧИСЛО невключительно
+            return items[randomInt];
+        }
+        else{
+            RuntimeException runtimeException = new RuntimeException("Коробка ещё не полна");
+        }
+        return null;
+    }
+
+    private boolean checkingForSpace () {
+        for (T item : items) {
+            if (item == null) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
